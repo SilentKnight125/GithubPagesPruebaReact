ba068211e7596e3000cc6a9a896fe73c375ccdc1
Walter Nina Maquera <gamervicio521@gmail.com>
2020-09-19 21:03:11 -0500

    Updates

---
+++ precache-manifest.04c1a3c0c5257a856ff1f34f8adfede5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "568768cad8f3f2bdf05901d33afb14ed",
+    "url": "/GithubPagesPruebaReact/index.html"
+  },
+  {
+    "revision": "3fad4ebc81535899e459",
+    "url": "/GithubPagesPruebaReact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a5567f761d182c6018d6",
+    "url": "/GithubPagesPruebaReact/static/js/2.96b30471.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GithubPagesPruebaReact/static/js/2.96b30471.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fad4ebc81535899e459",
+    "url": "/GithubPagesPruebaReact/static/js/main.63bf33e8.chunk.js"
+  },
+  {
+    "revision": "42045ddeb72cb0cb2cbe",
+    "url": "/GithubPagesPruebaReact/static/js/runtime-main.7f9eaf99.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/GithubPagesPruebaReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
